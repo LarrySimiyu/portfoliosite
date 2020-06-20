c6e8e7331b6e74783e4d49bb1943e9ab95a456db
LarrySimiyu <larry.simiyu@gmail.com>
2020-06-20 11:56:02 -0700

    navigation changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
 import React from 'react';
 import logo from './logo.svg';
 import '../src/styles/App.css';
-import Navigation from './components/Navigation'
+import Navbar from '../src/components/Navbar.js';
+import Section from '../src/components/Section.js';
+import dummyText from './DummyText.js'
 
 function App() {
   return (
     <div className="App">
-       <Navigation />
-       
+      <Navbar />
+      <Section
+          title="Section 1"
+          // subtitle={dummyText}
+          dark={true}
+          id="section1"
+        ><div>hello</div></Section>
+        <Section
+          title="Section 2"
+          subtitle={dummyText}
+          dark={false}
+          id="section2"
+        />
+        <Section
+          title="Section 3"
+          subtitle={dummyText}
+          dark={true}
+          id="section3"
+        />
+        <Section
+          title="Section 4"
+          subtitle={dummyText}
+          dark={false}
+          id="section4"
+        />
+        <Section
+          title="Section 5"
+          subtitle={dummyText}
+          dark={true}
+          id="section5"
+        />
+
     </div>
   );
 }
