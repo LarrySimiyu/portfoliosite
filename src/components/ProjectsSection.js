2e1798f9eb4f86719a7cbdaac909f7d909345781
LarrySimiyu <larry.simiyu@gmail.com>
2020-08-24 10:12:26 -0700

    cards created

--- src/components/ProjectsSection.js
+++ src/components/ProjectsSection.js
@@ -1,11 +1,62 @@
 import React from "react";
 
+import ProjectOne from "../images/IMG_1415.JPG"
+import ProjectTwo from "../images/IMG_1415.JPG"
+import ProjectThree from "../images/IMG_1415.JPG"
+import ProjectFour from "../images/IMG_1415.JPG"
+import ProjectFive from "../images/IMG_1415.JPG"
+
+
 export default function ProjectsSettings({ title, subtitle, id }) {
   return (
     <div className={"section"}>
       <div className="projects-content" id={id}>
         <h1>{title}</h1>
         <p>{subtitle}</p>
+
+        <div className="card">
+          <img src={ProjectOne} alt="Project" className="projectImage"/>
+          <div className="container">
+            <h4><b>Project Name Here</b></h4>
+            <p>Project Info</p>
+          </div>
+        </div>
+
+        <div className="card">
+          <img src={ProjectTwo} alt="Project" className="projectImage"/>
+          <div className="container">
+            <h4><b>Project Name Here</b></h4>
+            <p>Project Info</p>
+          </div>
+        </div>
+
+        <div className="card">
+          <img src={ProjectThree} alt="Project" className="projectImage"/>
+          <div className="container">
+            <h4><b>Project Name Here</b></h4>
+            <p>Project Info</p>
+          </div>
+        </div>
+
+        <div className="card">
+          <img src={ProjectFour} alt="Project" className="projectImage"/>
+          <div className="container">
+            <h4><b>Project Name Here</b></h4>
+            <p>Project Info</p>
+          </div>
+        </div>
+
+        <div className="card">
+          <img src={ProjectFive} alt="Project" className="projectImage"/>
+          <div className="container">
+            <h4><b>Project Name Here</b></h4>
+            <p>Project Info</p>
+          </div>
+        </div>
+
+      
+
+
       </div>
     </div>
   );
