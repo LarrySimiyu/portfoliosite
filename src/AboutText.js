6608b760436bc12bbba055bca994a1ed6161f11e
LarrySimiyu <larry.simiyu@gmail.com>
2021-03-22 16:27:44 -0700

    spelling fix

--- src/AboutText.js
+++ src/AboutText.js
@@ -1,7 +1,7 @@
 
 
 const aboutText = `I've worked as a Full Stack and iOS Developer. My days are spent either making
-videos about tech or coding. I love finding new ways to motivate others to code trough the content I create.
+videos about tech or coding. I love finding new ways to motivate others to code through the content I create.
 I look for a challenge that scares me at first, that type of challenge have helped me grow faster and learn more than
 I could ever expect to in such a short time.`
 
